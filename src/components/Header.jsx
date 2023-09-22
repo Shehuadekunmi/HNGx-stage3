91bbcbf49f9bceb49960ebc71ce36a0a641fb1dc
Shehuadekunmi <sirkumyemo@gmail.com>
2023-09-22 13:06:18 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
 import logo from '../assets/images.png'
 import SearchBar from './Searchbar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from './Firebaseconfig'
 
 
 const Header = () => {
+const redirect = useNavigate()
 
+const logout = () =>{
+  signOut(auth)
+  .then(() =>{
+    redirect('/')
+  })
+  .catch((error) => {
+    console.log('logout failed', error);
+  });
+};
  
   return (
     <div className='d-fle text-center  search'>
       <div className='log mx-2'> <Link to={'/'}><img src={logo} alt="" /> </Link>  </div>
-       <div > <SearchBar/> </div>
+       <div> <SearchBar /> </div>
+      <div className='btt'> <button onClick={logout} className='btn btn-primary mx-2'> Logout</button></div>
     </div>
   )
 }
