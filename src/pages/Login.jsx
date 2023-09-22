077687e3eeead5ae76eef52ca509c1d99af5c475
Shehuadekunmi <sirkumyemo@gmail.com>
2023-09-22 20:00:29 +0100

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,94 +1,99 @@
-import React, { useEffect, useState } from 'react'
-import Loading from '../components/Loading'
-import {RxCross2} from 'react-icons/rx'
-import '../styles/signup.css'
-import { Link, useNavigate } from 'react-router-dom'
-import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
-import { auth } from '../components/Firebaseconfig'
-import { ToastContainer, toast } from 'react-toastify'
-
+import React, { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import '../styles/signup.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '../components/Firebaseconfig';
 
 const Login = () => {
-    const [loading, setLoading] = useState(false)
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [user, setUser] = useState({})
-    const redirect = useNavigate()
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({});
+  const [error, setError] = useState(null);
+  const redirect = useNavigate();
 
-
-    useEffect(()=>{
-      const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-      });
-      return ()=>{
-        unsubscribe()
-      }
-    }, [])
-   
-
-    const login = async (e) => {
-      e.preventDefault()
-      try {
-        const user = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-  redirect('/drop')
-  toast.success('Login successful')
-
-        console.log(user);
-      } catch (error) {
-        console.log(error.message);
-        toast.error('error occure while login please try agian')
-        if(error.status=404){
-          alert("invalid email");
-       }
-      }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
     };
-  
-    const logout = async () => {
-      await signOut(auth);
-    };
+  }, []);
 
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      redirect('/drop');
+      console.log(userCredential);
+    } catch (error) {
+      console.error(error.message);
+      if (error.code === 'auth/wrong-password') {
+        setError('Incorrect password. Please check your password.');
+      } else if (error.code === 'auth/user-not-found') {
+        setError('Invalid email address. Please check your email.');
+      } else {
+        setError('An error occurred while logging in. Please try again later.');
+      }
+    } finally {
+      setLoading(true);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
 
   return (
     <div className="bg container">
-    <div className='hei py-5'>
-    <form  className="text-center   py-5 log form">
-    <Link to='/'> <RxCross2 className='cross'/> </Link>
-      <div  className='my-4'>
-        <label htmlFor="email">Email</label> <br />
-        <input
-          type="email"
-          id="email"
-          required
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-      </div>
-      <div  className='my-4'>
-        <label htmlFor="password">Password</label> <br />
-        <input
-          type="password"
-          id="password"
-          required
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-      </div>
-      <button type="submit" onClick={login} className="btn btn-primary">{loading ? <Loading/> : "Continue"}</button>
-      <p className="pt-3">No account? <Link to={'/signup'}>Sign Up</Link> </p>
-      <h4> User Logged In: </h4>
-      {user?.email}
-<br />
-      <button className='btn btn-secondary' onClick={logout}> Sign Out </button>
-    </form>
-    </div>
-  </div>
-  )
-}
+      <div className="hei py-5">
+        <form className="text-center py-5 log form">
+          
+          <div className="my-4">
+            <label htmlFor="email">Email</label> <br />
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={(event) => {
+                setEmail(event.target.value);
+                setError(null); 
+              }}
+            />
+          </div>
+          <div className="my-4">
+            <label htmlFor="password">Password</label> <br />
+            <input
+              type="password"
+              id="password"
+              required
+              onChange={(event) => {
+                setPassword(event.target.value);
+                setError(null); 
+              }}
+            />
+          </div>
+          {error && <p className="error text-danger">{error}</p>}
 
-export default Login
+          <button type="submit" onClick={login} className="btn btn-primary">
+            {loading ? <Loading /> : 'Continue'}
+          </button>
+          <p className="pt-3">
+            No account? <Link to={'/signup'}>Sign Up</Link>{' '}
+          </p>
+          <h4> User Logged In: </h4>
+          {user?.email}
+          <br />
+          <button className="btn btn-secondary" onClick={logout}>
+            {' '}
+            Sign Out{' '}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,90 +1,98 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {RxCross2} from 'react-icons/rx'
-import Loading from '../components/Loading'
-import '../styles/signup.css'
-import { createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
-import {auth} from '../components/Firebaseconfig'
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+// import { RiClose2Line } from 'react-icons/ri';
+import Loading from '../components/Loading';
+import '../styles/signup.css';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../components/Firebaseconfig';
 
 const Signup = () => {
-  const redirect = useNavigate()
+  const redirect = useNavigate();
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState({})
-
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   const createUser = async (e) => {
     e.preventDefault();
-    setLoading(true)
+    setLoading(true);
 
     try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      if(password.length < 5){
-            errors.password = 'password length must be greater than 6'
-          }
-        redirect('/login')
+      if (password.length < 6) {
+        setErrors({ password: 'Password length must be at least 6 characters' });
+        setLoading(false);
+        return;
+      }
 
-      console.log(user);
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      redirect('/login');
+      console.log(userCredential);
     } catch (error) {
-      console.log(error.message);
-      if(error.status=404){
-        alert('password length must be greater than 6');
-     }
+      console.error(error.message);
 
+      if (error.code === 'auth/email-already-in-use') {
+        setErrors({ email: 'Email address is already registered' });
+      } else {
+        setErrors({ general: 'An error occurred during registration' });
+      }
+
+      setLoading(false);
     }
 
-    setEmail('')
-    setPassword('')
-    setLoading(false)
+    setEmail('');
+    setPassword('');
   };
-
- 
 
   return (
     <div>
+      <div className="bg container">
+        <div className="hei pt-4 pt-md-5">
+          <form onSubmit={createUser} className="text-center log form">
 
-<div className='bg container '>
-     
-     <div className='hei pt-4 pt-md-5'>
-      <form onSubmit={createUser} className='text-center log  form' >
-     <Link to='/'> <RxCross2 className='cross'/> </Link>
+            <p className="px-2">Enter your email address to create an account on Image Gallery.</p>
 
+            <div className="my-4">
+              <label>Your Email Address</label> <br />
+              <input
+                type="email"
+                required
+                placeholder="Enter your email"
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+                value={email}
+              />
+              {errors.email && <p className="error-message text-danger">{errors.email}</p>}
+            </div>
 
-      <p className=' px-2'>Enter your email address to create an account on Image Gallery.</p>
-       
-        <div className='my-4'>
-          <label>Your Email Address</label> <br />
-          <input type="email"  required placeholder='enter your email'
-         onChange={(event) => {
-          setEmail(event.target.value);
-        }}
-        value={email}
-          />
+            <div className="my-4">
+              <label htmlFor="password">Password</label> <br />
+              <input
+                type="password"
+                id="password"
+                required
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+                value={password}
+              />
+              {errors.password && <p className="error-message text-danger">{errors.password}</p>}
+            </div>
+
+            {errors.general && <p className="error-message text-danger">{errors.general}</p>}
+
+            <button type="submit" className="btn btn-primary mb-3">
+              {loading ? <Loading /> : 'Continue'}
+            </button>
+
+            <p className="pb-2">
+              Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+          </form>
         </div>
-        <div className='my-4'>
-          <label htmlFor="password">Password</label> <br />
-          <input type="password" id='password'  required
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-          value={password}
-          />
-
-        </div>
-        <button type='submit'   className='btn btn-primary mb-3'>{loading ? <Loading/> : "Continue"}</button>
-        <p className='pb-2'>Already have an account? <Link to={'/login'}>Sign In</Link></p>
-      </form>
-     
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
